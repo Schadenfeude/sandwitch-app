d1597df6ea795da8ae796174dadb21683cbe14db
Daniel Milkov <daniel.milkov@wacom.eu>
2018-02-27 18:32:27 +0200

    Sandwich app v1

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,26 +1,32 @@
 package com.udacity.sandwichclub;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
+
     private static final int DEFAULT_POSITION = -1;
+    private static final String DELIMITER = ", ";
+    private static final String LAST_DELIMITER = " and ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,12 +49,8 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
-
         setTitle(sandwich.getMainName());
+        populateUI(sandwich);
     }
 
     private void closeOnError() {
@@ -56,7 +58,59 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        final ImageView sandwichImageIv = findViewById(R.id.image_iv);
+        final TextView placeOfOriginTv = findViewById(R.id.origin_tv);
+        final TextView alsoKnownAsTv = findViewById(R.id.also_known_tv);
+        final TextView descriptionTv = findViewById(R.id.description_tv);
+        final TextView ingredientsTv = findViewById(R.id.ingredients_tv);
 
+        Picasso.with(this)
+                .load(sandwich.getImage())
+                .into(sandwichImageIv);
+        placeOfOriginTv.setText(sandwich.getPlaceOfOrigin());
+        descriptionTv.setText(sandwich.getDescription());
+        populateTextFromList(alsoKnownAsTv, sandwich.getAlsoKnownAs());
+        populateTextFromList(ingredientsTv, sandwich.getIngredients());
+    }
+
+    private void populateTextFromList(TextView textView, List<String> stringList) {
+        if (stringList.isEmpty()) {
+            return;
+        }
+
+        if (stringList.size() > 1) {
+            textView.append(makeStringFromList(stringList));
+        } else {
+            textView.append(stringList.get(0));
+        }
+    }
+
+    private String makeStringFromList(List<String> stringList) {
+        final String lastString = LAST_DELIMITER + stringList.get(stringList.size() - 1);
+        final StringBuilder formattedText = new StringBuilder();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            formattedText.append(String.join(DELIMITER, stringList));
+            formattedText.insert(formattedText.lastIndexOf(DELIMITER), lastString);
+            formattedText.delete(formattedText.lastIndexOf(DELIMITER), formattedText.length());
+        } else {
+            final Iterator<String> itemIterator = stringList.iterator();
+            String nextItem;
+
+            while (itemIterator.hasNext()) {
+                nextItem = itemIterator.next();
+                if (!itemIterator.hasNext()) {
+                    formattedText.append(nextItem);
+                } else {
+                    formattedText.append(nextItem).append(DELIMITER);
+                }
+            }
+
+            formattedText.insert(formattedText.lastIndexOf(DELIMITER), lastString);
+            formattedText.delete(formattedText.lastIndexOf(DELIMITER), formattedText.length());
+        }
+
+        return formattedText.toString();
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,63 @@
 package com.udacity.sandwichclub.utils;
 
+import android.support.annotation.NonNull;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
+    private static final String NAME_KEY = "name";
+    private static final String MAIN_NAME_KEY = "mainName";
+    private static final String AKA_NAME_KEY = "alsoKnownAs";
+    private static final String PLACE_OF_ORIGIN_KEY = "placeOfOrigin";
+    private static final String DESCRIPTION_KEY = "description";
+    private static final String IMAGE_KEY = "image";
+    private static final String INGREDIENTS_KEY = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich = null;
+
+        try {
+            final JSONObject sandwichJson = new JSONObject(json);
+
+            final JSONObject nameJson = sandwichJson.getJSONObject(NAME_KEY);
+            final String mainName = nameJson.getString(MAIN_NAME_KEY);
+            final List<String> aka = jsonArrayToStringList(nameJson.getJSONArray(AKA_NAME_KEY));
+
+            final String placeOfOrigin = sandwichJson.getString(PLACE_OF_ORIGIN_KEY);
+            final String description = sandwichJson.getString(DESCRIPTION_KEY);
+            final String image = sandwichJson.getString(IMAGE_KEY);
+
+            final JSONArray ingredientsJson = sandwichJson.getJSONArray(INGREDIENTS_KEY);
+            final List<String> ingredients = jsonArrayToStringList(ingredientsJson);
+
+            sandwich = new Sandwich(mainName, aka, placeOfOrigin, description, image, ingredients);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return sandwich;
+    }
+
+    @NonNull
+    private static ArrayList<String> jsonArrayToStringList(final JSONArray jsonArray) {
+        ArrayList<String> stringList = new ArrayList<>(jsonArray.length());
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            try {
+                stringList.add(jsonArray.getString(i));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return stringList;
     }
 }
